24f1cb24304bb69d039d24af07fe59408c9eb118
codewilson64 <wilsongambit@gmail.com>
2024-03-07 14:24:30 +0700

    add: hashrouter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
